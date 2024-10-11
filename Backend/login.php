6e6cafa5e8f8fe87f5c5160e5045d0af613c73a3
Renzzzz-Git <gdevrenzz055@gmail.com>
2024-10-11 22:32:19 +0800

    login and logout with sessions completed

---
+++ Backend/login.php
@@ -0,0 +1,20 @@
+<?php 
+    //LOGIN CODE FOR CONNECTING USER TO DATABASE
+	if(ISSET ($_POST['user_login'])){
+		$username = $_POST['username'];
+		$password = $_POST['password'];
+        session_start();
+        $conn = mysqli_connect('localhost', 'root', '', 'voting_system') or die('Unable to connect');
+		$query = $conn->query("SELECT * FROM `user` WHERE `username` = '$username' && `password` = '$password'") or die(mysqli_error());
+		$fetch = $query->fetch_array();
+		$row = $query->num_rows;
+		
+		if($row > 0){
+			$_SESSION['username'] = $fetch['username'];
+			$_SESSION['userID'] = $fetch['userID'];
+			header('location:homepage.php');
+		}else{
+			echo "<center><label style = 'color:red;'>Invalid username or password</label></center>";
+		}
+	}
+?>
