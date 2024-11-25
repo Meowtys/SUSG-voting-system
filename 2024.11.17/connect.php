37d5cfa076be73a51587cdb282df60f88f487a63
Meowtys <johnadasig@su.edu.ph>
2024-11-25 18:40:29 +0800

    updated db connection

--- 2024.11.17/connect.php
+++ 2024.11.17/connect.php
@@ -1,15 +1,7 @@
-<?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "your_database_name";
+<?php 
+$host="localhost";
+$dbname='auth';
+$username='root';
+$password='';
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-echo "Connected successfully";
-?>
+$pdo=new PDO("mysql:host=$host;dbname=$votesusg",$username,$password);
