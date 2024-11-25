24007fc1665fe30701cc56d691f11706f4351c6f
Meowtys <johnadasig@su.edu.ph>
2024-11-25 20:10:39 +0800

    made session keep track of user details

--- 2024.11.17/user-session.php
+++ 2024.11.17/user-session.php
@@ -42,6 +42,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['signin'])) {
     if ($user && $password === $user['password']) {
         $_SESSION['user'] = [
             'student_id' => $user['student_id'],
+            'name' => $user['name'],
+            'college_id' => $user['college_id'],
+            'has_voted' => $user['has_voted'],
         ];
 
         header('Location: homepage.php');
