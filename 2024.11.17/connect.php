3c54c345e196b011f0ff94de30609606975ee1b5
Meowtys <johnadasig@su.edu.ph>
2024-11-26 07:56:24 +0800

    skeptical: change db connection code

--- 2024.11.17/connect.php
+++ 2024.11.17/connect.php
@@ -4,4 +4,10 @@ $dbname='votesusg';
 $username='root';
 $password='';
 
-$pdo=new PDO("mysql:host=$host;dbname=$dbname",$username,$password);
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
+}
+?>
