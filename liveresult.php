8cdc0acf41f3b2237edadce68740ac99c7113a5c
Meowtys <johnadasig@su.edu.ph>
2024-12-14 05:48:45 +0800

    Just some typographical fix

--- liveresult.php
+++ liveresult.php
@@ -502,7 +502,7 @@ foreach ($candidates as $candidate) {
                         <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
                         <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
                     </svg>
-                    <span>Refreshing in 30s</span>
+                    <span>Auto-refreshing in 30s</span>
                 </div>
                 
                 <div class="flex flex-col">
@@ -633,7 +633,7 @@ foreach ($candidates as $candidate) {
 
             const timer = setInterval(() => {
                 countdown--;
-                timerSpan.textContent = `Auto-Refreshing in ${countdown}s`;
+                timerSpan.textContent = `Auto-refreshing in ${countdown}s`;
                 if (countdown <= 0) clearInterval(timer);
             }, 1000);
         }
