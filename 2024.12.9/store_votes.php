d2de4c7b86fa1fe8eb9ecac2345bda96cb74fe6d
Meowtys <johnadasig@su.edu.ph>
2024-12-14 02:09:26 +0800

    finalized the main

--- 2024.12.9/store_votes.php
+++
@@ -1,9 +0,0 @@
-<?php
-if (session_status() == PHP_SESSION_NONE) {
-    session_start();
-}
-
-$data = json_decode(file_get_contents('php://input'), true);
-$_SESSION['selectedVotes'] = $data;
-
-echo json_encode(['success' => true]);
