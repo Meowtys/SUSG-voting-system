2c3890c543109219d67787893cd1496bcb8bdc55
Meowtys <johnadasig@su.edu.ph>
2024-12-13 08:39:11 +0800

    log-out destroys separate sessions

--- logout.php
+++ logout.php
@@ -1,8 +1,30 @@
 <?php
 session_start();
-session_unset();
-session_destroy();
-header("location: start.php");
+
+// Check which type of logout was requested
+if (isset($_GET['type'])) {
+    if ($_GET['type'] === 'voter') {
+        // Only clear voter session data
+        unset($_SESSION['user']);
+        header("Location: loginasvoter.php");
+    } elseif ($_GET['type'] === 'comelec') {
+        // Only clear comelec session data
+        unset($_SESSION['comelec_name']);
+        unset($_SESSION['is_comelec_logged_in']);
+        header("Location: loginascomelec.php");
+    } else {
+        // Invalid logout type - logout completely
+        session_unset();
+        session_destroy();
+        header("Location: start.php");
+    }
+} else {
+    // No type specified - logout completely
+    session_unset();
+    session_destroy();
+    header("Location: start.php");
+}
+exit();
 ?>
 
 
