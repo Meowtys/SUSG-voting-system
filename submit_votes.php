ebff5180fb3bcf1e7b79b635626d650533218498
Meowtys <johnadasig@su.edu.ph>
2024-12-15 15:11:44 +0800

    fixed auto generate abstain with unqualified

--- submit_votes.php
+++ submit_votes.php
@@ -70,7 +70,7 @@ try {
                             qualified, election_id, party_id
                         ) VALUES (
                             'Abstain', 0, :position_id, 
-                            1, :election_id, 3
+                            0, :election_id, 3
                         )
                     ");
                     $createAbstainStmt->execute([
@@ -159,7 +159,7 @@ try {
                             qualified, election_id, party_id
                         ) VALUES (
                             'Abstain', 0, :position_id, 
-                            1, :election_id, 3
+                            0, :election_id, 3
                         )
                     ");
                     $createAbstainStmt->execute([
